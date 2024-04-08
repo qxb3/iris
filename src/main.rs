2a5f42b8f7538cb00d73ccc2cdb5f57c9db2b44c
qxb3 <qxbthree@gmail.com>
2024-04-08 20:36:41 +0800

    cli wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,39 @@
+use clap::{command, Command, Parser};
+
 mod server;
 mod client;
 
-fn main() {
-    let addr = "127.0.0.1:3000";
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
 
-    server::start(addr);
-    client::start(addr);
+}
+
+fn main() {
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("server", _sub_matches)) => {
+            server::start("127.0.0.1:3000");
+        },
+        Some(("client", _sub_matches)) => {
+            client::start("127.0.0.1:3000");
+        },
+        _ => unreachable!()
+    }
+
+}
+
+fn cli() -> Command {
+    Command::new("iris")
+        .version(env!("CARGO_PKG_VERSION"))
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("server")
+                .about("Start the in-memory server")
+        )
+        .subcommand(
+            Command::new("client")
+                .about("Enter client repl mode")
+        )
 }
