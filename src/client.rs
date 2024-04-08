e1249e20273c6161abfcd01bc66f57a9397a7979
qxb3 <qxbthree@gmail.com>
2024-04-09 00:56:26 +0800

    client - remove panic and use process::exit() instead

--- src/client.rs
+++ src/client.rs
@@ -1,10 +1,13 @@
-use std::{io::Write, net::TcpStream};
+use std::{io::Write, net::TcpStream, process};
 
 pub fn start(addr: &str) {
     match TcpStream::connect(addr) {
         Ok(mut stream) => {
             stream.write_all("test\nfoo".as_bytes()).ok();
         }
-        Err(err) => panic!("failed to connect: {err}"),
+        Err(err) => {
+            println!("Failed to connect: {err}");
+            process::exit(1);
+        },
     }
 }
