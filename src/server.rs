8c4a50d7a45d170a2ca822aa0b332a56b2311544
qxb3 <qxbthree@gmail.com>
2024-04-09 01:56:07 +0800

    server - handle binding error

--- src/server.rs
+++ src/server.rs
@@ -1,50 +1,55 @@
 use std::{
-    io::{BufRead, BufReader, Write},
-    net::TcpListener,
-    thread,
+    io::{BufRead, BufReader, Write}, net::TcpListener, process, thread
 };
 
 use indoc::indoc;
 
 pub fn start(addr: &str) {
-    let listener = TcpListener::bind(addr).unwrap();
+    match TcpListener::bind(addr) {
+        Ok(listener) => {
+            let local_addr = listener.local_addr().unwrap();
 
-    let local_addr = listener.local_addr().unwrap();
-    println!(
-        indoc! {"
-              ▀             ▀
-            ▄▄▄     ▄ ▄▄  ▄▄▄     ▄▄▄
-              █     █▀  ▀   █    █   ▀
-              █     █       █     ▀▀▀▄
-            ▄▄█▄▄   █     ▄▄█▄▄  ▀▄▄▄▀
+            println!(
+                indoc! {"
+                      ▀             ▀
+                    ▄▄▄     ▄ ▄▄  ▄▄▄     ▄▄▄
+                      █     █▀  ▀   █    █   ▀
+                      █     █       █     ▀▀▀▄
+                    ▄▄█▄▄   █     ▄▄█▄▄  ▀▄▄▄▀
 
-            Server has started.
-            • version:  {},
-            • host:     http://{}
-            • port:     {}
-        "},
-        env!("CARGO_PKG_VERSION"),
-        local_addr,
-        local_addr.port()
-    );
+                    Server has started.
+                    • version:  {},
+                    • host:     http://{}
+                    • port:     {}
+                "},
+                env!("CARGO_PKG_VERSION"),
+                local_addr,
+                local_addr.port()
+            );
 
-    for incoming in listener.incoming() {
-        thread::spawn(|| {
-            let mut stream = incoming.unwrap();
-            let buf_reader = BufReader::new(&mut stream);
-            let req: Vec<String> = buf_reader
-                .lines()
-                .map(|result| result.unwrap())
-                .take_while(|line| !line.is_empty())
-                .collect();
+            for incoming in listener.incoming() {
+                thread::spawn(|| {
+                    let mut stream = incoming.unwrap();
+                    let buf_reader = BufReader::new(&mut stream);
+                    let req: Vec<String> = buf_reader
+                        .lines()
+                        .map(|result| result.unwrap())
+                        .take_while(|line| !line.is_empty())
+                        .collect();
 
-            println!("Req: {:#?}", req);
+                    println!("Req: {:#?}", req);
 
-            stream
-                .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
-                .unwrap();
-        });
+                    stream
+                        .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                        .unwrap();
+                });
+            }
+
+            drop(listener);
+        },
+        Err(err) => {
+            println!("Failed to start server at {addr}: {err}");
+            process::exit(1);
+        }
     }
-
-    drop(listener);
 }
