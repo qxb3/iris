8c5e886c4cf47c9e225cff238db0427fe133f384
qxb3 <qxbthree@gmail.com>
2024-04-10 07:54:36 +0800

    rewrite client code to tokio async

--- src/client.rs
+++ src/client.rs
@@ -1,102 +1,86 @@
-use std::{
-    io::{BufRead, BufReader, Write},
-    net::TcpStream,
-    process,
-};
-
+use std::{io::{self, Write}, process};
+use tokio::{io::{AsyncBufReadExt, AsyncWriteExt, BufReader}, net::TcpStream};
 use indoc::{indoc, printdoc};
 
-macro_rules! write_error {
-    ($message:expr) => {
-        {
-            println!("{}\n", $message);
-            continue;
-        }
-    };
-}
-
-macro_rules! handle_reply {
-    ($stream:expr) => {
-        {
-            let mut buf_reader = BufReader::new(&mut $stream);
-
-            let mut buffer = String::new();
-            match buf_reader.read_line(&mut buffer) {
-                Ok(byte) => {
-                    if byte == 0 {
-                        println!("Connection closed.");
-                        process::exit(1);
-                    }
-
-                    println!("> {buffer}");
-                },
-                Err(err) => write_error!(format!("> Failed: {err}"))
-            }
-        }
-    };
-}
-
 pub async fn start(addr: &str) {
-    match TcpStream::connect(addr) {
-        Ok(mut stream) => {
-            let local_addr = &stream.peer_addr().unwrap();
-            println!(
-                indoc! {"
-
-                      ▀             ▀
-                    ▄▄▄     ▄ ▄▄  ▄▄▄     ▄▄▄
-                      █     █▀  ▀   █    █   ▀
-                      █     █       █     ▀▀▀▄
-                    ▄▄█▄▄   █     ▄▄█▄▄  ▀▄▄▄▀
-
-                    Client is connected.
-                    • version:  {}
-                    • host:     http://{}
-                    • port:     {}
-                "},
-                env!("CARGO_PKG_VERSION"),
-                local_addr,
-                local_addr.port()
-            );
-
-            loop {
-                if let Ok(line) = readline() {
-                    if line.is_empty() {
-                        continue;
-                    }
-
-                    let command = line.clone().splitn(3, ' ').collect::<Vec<&str>>().get(0).unwrap().to_lowercase();
-                    match command.as_str() {
-                        "help" => printdoc! {"
-                            • What is iris?
-                              iris is a simple key value database,
-                              every value in iris is considered to be a string (for now)
-                              and you, yourself will be the one to parse the types.
-
-                            • Commands
-                              - SET <id:number> <data:string>: sets a value on a key.
-                              - GET <id:number>: sets a value on a key.
-                              - DEL <id:number>: deletes a value on a key.
-                              - help: how this message.
-                              - clear: clear prompt.
-                              - exit: exit repl.
-                        "},
-                        "clear" => print!("\x1B[2J\x1B[1;1H"),
-                        "exit" => process::exit(0),
-                        _ => {
-                            match stream.write_all(format!("{}\n", line).as_bytes()) {
-                                Ok(_) => handle_reply!(stream),
-                                Err(err) => write_error!(format!("Failed: {err}"))
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    let mut stream = match TcpStream::connect(addr).await {
+        Ok(stream) => stream,
         Err(err) => {
             println!("Failed to connect: {err}");
             process::exit(1);
-        },
+        }
+    };
+
+    let local_addr = &stream.peer_addr().unwrap();
+
+    println!(
+        indoc! {"
+
+              ▀             ▀
+            ▄▄▄     ▄ ▄▄  ▄▄▄     ▄▄▄
+              █     █▀  ▀   █    █   ▀
+              █     █       █     ▀▀▀▄
+            ▄▄█▄▄   █     ▄▄█▄▄  ▀▄▄▄▀
+
+            Client is connected.
+            • version:  {}
+            • host:     http://{}
+            • port:     {}
+        "},
+        env!("CARGO_PKG_VERSION"),
+        local_addr,
+        local_addr.port()
+    );
+
+    loop {
+        let line = match readline() {
+            Ok(line) => line,
+            Err(err) => {
+                println!("Failed prompt: {err}");
+                process::exit(1);
+            }
+        };
+
+        let command = line.clone().splitn(3, ' ').collect::<Vec<&str>>().get(0).unwrap().to_lowercase();
+        match command.as_str() {
+            "help" => printdoc! {"
+                • What is iris?
+                  iris is a simple key value database,
+                  every value in iris is considered to be a string (for now)
+                  and you, yourself will be the one to parse the types.
+
+                • Commands
+                 - SET <id:number> <data:string>: sets a value on a key.
+                 - GET <id:number>: sets a value on a key.
+                 - DEL <id:number>: deletes a value on a key.
+                 - help: how this message.
+                 - clear: clear prompt.
+                 - exit: exit repl.
+            "},
+            "clear" => print!("\x1B[2J\x1B[1;1H"),
+            "exit" => process::exit(0),
+            _ => {}
+        }
+
+        if let Err(err) = stream.write_all(format!("{line}\n").as_bytes()).await {
+            println!("Failed to send: {err}");
+            continue;
+        }
+
+        let mut buffer = String::new();
+        let server_resp = match BufReader::new(&mut stream).read_line(&mut buffer).await {
+            Ok(0) => {
+                println!("Connection closed.");
+                process::exit(1);
+            },
+            Ok(_) => buffer,
+            Err(err) => {
+                println!("> Failed to read stream: {err}");
+                continue;
+            }
+        };
+
+        println!("> {server_resp}");
     }
 }
 
