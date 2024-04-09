d9b0ee1861185a1ad5db12ea297e79fe753efc2a
qxb3 <qxbthree@gmail.com>
2024-04-09 12:55:25 +0800

    server - new way of doing things & added get & set commands

--- src/server.rs
+++ src/server.rs
@@ -5,25 +5,20 @@ use std::{
 use indoc::indoc;
 
 macro_rules! write_error {
-    ($stream:expr, $data:expr) => {
+    ($stream:expr, $message:expr) => {
         {
-            let error_message = format!("Status: Err\nData: {:?}\r\n\r\n", $data);
-            $stream.write_all(error_message.as_bytes()).unwrap();
-            return;
+            $stream.write_all(format!("{}\n", $message).as_bytes()).unwrap();
+            continue;
         }
     };
 }
 
-trait ParseLine {
-    fn parse_line(&self) -> Option<(String, String)>;
-}
-
-impl ParseLine for String {
-    fn parse_line(&self) -> Option<(String, String)> {
-        self
-            .split_once(':')
-            .map(|(key, value)| (key.trim().to_string(), value.trim().to_string()))
-    }
+macro_rules! debug {
+    ($message:expr, $condition:expr) => {
+        if $condition {
+            println!("{}", $message);
+        }
+    };
 }
 
 pub fn start(addr: &str, debug: bool) {
@@ -40,7 +35,7 @@ pub fn start(addr: &str, debug: bool) {
                     ▄▄█▄▄   █     ▄▄█▄▄  ▀▄▄▄▀
 
                     Server has started.
-                    • version:  {},
+                    • version:  {}
                     • host:     http://{}
                     • port:     {}
                 "},
@@ -49,60 +44,85 @@ pub fn start(addr: &str, debug: bool) {
                 local_addr.port()
             );
 
-            let iris: Arc<Mutex<HashMap<u32, String>>> = Arc::new(Mutex::new(HashMap::new()));
+            let db: Arc<Mutex<HashMap<u32, String>>> = Arc::new(Mutex::new(HashMap::new()));
 
             for incoming in listener.incoming() {
-                let iris_clone = Arc::clone(&iris);
+                let db_clone = Arc::clone(&db);
 
                 thread::spawn(move || {
-                    let mut iris = iris_clone.lock().unwrap();
+                    let mut db = db_clone.lock().unwrap();
 
                     let mut stream = incoming.unwrap();
-                    let buf_reader = BufReader::new(&mut stream);
-                    let request: HashMap<String, String> = buf_reader
-                        .lines()
-                        .map(|result| result.unwrap())
-                        .take_while(|line| line != "End")
-                        .filter(|line| line.parse_line().is_some())
-                        .map(|line| {
-                            let (key, value) = line.parse_line().unwrap();
-                            (key.to_lowercase(), value)
-                        })
-                        .collect();
 
-                    if debug {
-                        println!("Request: {:#?}", request);
-                    }
+                    loop {
+                        let mut buf_reader = BufReader::new(&mut stream);
 
-                    let command = match request.get("command") {
-                        Some(command) => command.to_uppercase(),
-                        None => write_error!(stream, "Cannot find \"Command\"")
-                    };
+                        let mut buffer = String::new();
+                        match buf_reader.read_line(&mut buffer) {
+                            Ok(byte) => {
+                                if byte == 0 {
+                                    debug!("Connection closed", debug);
+                                    break;
+                                }
 
-                    let id = match request.get("id") {
-                        Some(id) => match id.parse::<u32>() {
-                            Ok(id) => id,
-                            Err(_) => write_error!(stream, "ID needs to be a number")
-                        },
-                        None => write_error!(stream, "Cannot find \"ID\"")
-                    };
+                                let mut parts = buffer
+                                    .splitn(3, ' ')
+                                    .map(|part| part.trim());
 
-                    let data: Option<String> = match request.get("data") {
-                        Some(data) => Some(data.to_owned()),
-                        None => {
-                            if command == "SET" || command == "UPD" {
-                                write_error!(stream, format!("\"Data\" is required for \"{command}\""))
-                            } else {
-                                None
-                            }
+                                let command = match parts.next() {
+                                    Some(command) => command.to_uppercase(),
+                                    None => write_error!(stream, "No command specified")
+                                };
+
+                                let id = match parts.next() {
+                                    Some(id) => match id.parse::<u32>() {
+                                        Ok(id) => id,
+                                        Err(_) => write_error!(stream, "ID needs to be a number")
+                                    },
+                                    None => write_error!(stream, "No ID specified. \"<command> <id> [data]\"")
+                                };
+
+                                let data: Option<String> = match parts.next() {
+                                    Some(data) => Some(data.to_string()),
+                                    None => {
+                                        if command == "SET" || command == "UPD" {
+                                            write_error!(stream, format!("<data> is required for \"{command}\". <command> <id> <data>"))
+                                        } else {
+                                            None
+                                        }
+                                    }
+                                };
+
+                                debug!(format!(
+                                    indoc! {"
+                                        Request:
+                                            - Command: {}
+                                            - ID:      {}
+                                            - Data:    {:?}
+                                    "},
+                                    command,
+                                    id,
+                                    data
+                                ), debug);
+
+                                match command.as_str() {
+                                    "GET" => {
+                                        let result = match db.get(&id) {
+                                            Some(item) => item,
+                                            None => write_error!(stream, format!("Cannot find item with an id of \"{id}\""))
+                                        };
+
+                                        stream.write_all(format!("{result}\n").as_bytes()).unwrap();
+                                    },
+                                    "SET" => {
+                                        db.insert(id, data.unwrap());
+                                    }
+                                    _ => {}
+                                }
+                            },
+                            Err(err) => println!("Failed: {err}")
                         }
-                    };
-
-                    println!("{command} {id} {:?}", data);
-
-                    stream
-                        .write_all("Reply Babyyyyyyy\nEnd\n".as_bytes())
-                        .unwrap();
+                    }
                 });
             }
 
